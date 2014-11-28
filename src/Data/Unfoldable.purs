1b1b8a3750fa1c2e5fbf2dbe36e5d1e7487f90d4
Phil Freeman <paf31@cantab.net>
2014-11-27 16:34:17 -0800

    Fix build for new ST

--- src/Data/Unfoldable.purs
+++ src/Data/Unfoldable.purs
@@ -2,30 +2,23 @@ module Data.Unfoldable where
 
 import Data.Maybe
 import Data.Tuple
+import Data.Array.ST
 import Control.Monad.Eff
 import Control.Monad.ST
 
 class Unfoldable t where
   unfoldr :: forall a b. (b -> Maybe (Tuple a b)) -> b -> t a
  
-foreign import newEmptySTArray 
-  "function newEmptySTArray() {\
-  \  return [];\
-  \}" :: forall eff h a. Eff (st :: ST h | eff) (STArray h a)
-
 instance unfoldableArray :: Unfoldable [] where
   unfoldr f b = runPure (runSTArray (do
-    arr  <- newEmptySTArray
+    arr  <- emptySTArray
     seed <- newSTRef b
-    idx  <- newSTRef 0
     untilE $ do
       b1 <- readSTRef seed
       case f b1 of
         Nothing -> return true
         Just (Tuple a b2) -> do
-          i <- readSTRef idx
-          pokeSTArray arr i a
+          pushSTArray arr a
           writeSTRef seed b2
-          writeSTRef idx (i + 1)
           return false
     return arr))
